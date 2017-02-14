300440b3ab91ecc32e6e7ba923ea6aebacc19584
Zireael07 <zi@mail.com>
2017-02-14 10:14:48 +0100

    test STI map

---
+++ data/maps/arena_isometric.lua
@@ -0,0 +1,63 @@
+return {
+  version = "1.1",
+  luaversion = "5.1",
+  tiledversion = "0.18.1",
+  orientation = "isometric",
+  renderorder = "right-down",
+  width = 10,
+  height = 10,
+  tilewidth = 54,
+  tileheight = 28,
+  nextobjectid = 1,
+  properties = {},
+  tilesets = {
+    {
+      name = "gfx_isometric",
+      firstgid = 1,
+      tilewidth = 54,
+      tileheight = 54,
+      spacing = 0,
+      margin = 0,
+      image = "gfx_isometric.png",
+      imagewidth = 1404,
+      imageheight = 1998,
+      transparentcolor = "#ff00ff",
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      properties = {},
+      terrains = {},
+      tilecount = 962,
+      tiles = {}
+    }
+  },
+  layers = {
+    {
+      type = "tilelayer",
+      name = "Warstwa Kafelków 1",
+      x = 0,
+      y = 0,
+      width = 10,
+      height = 10,
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      properties = {},
+      encoding = "lua",
+      data = {
+        20, 877, 877, 877, 877, 877, 877, 877, 877, 929,
+        721, 210, 210, 210, 210, 210, 210, 210, 210, 721,
+        721, 210, 210, 210, 210, 210, 210, 210, 210, 721,
+        721, 210, 210, 210, 210, 210, 210, 210, 210, 721,
+        721, 210, 210, 210, 210, 210, 210, 210, 210, 721,
+        721, 210, 210, 210, 210, 210, 210, 210, 210, 721,
+        721, 210, 210, 210, 210, 210, 210, 210, 210, 721,
+        721, 210, 210, 210, 210, 210, 210, 210, 210, 721,
+        721, 210, 210, 210, 210, 210, 210, 210, 210, 721,
+        669, 877, 877, 877, 877, 877, 877, 877, 877, 773
+      }
+    }
+  }
+}
