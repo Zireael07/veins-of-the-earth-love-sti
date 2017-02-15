20c6e44b35a9f69dbdad42a614b7a5146f5a4d39
Zireael07 <zi@mail.com>
2017-02-15 19:50:26 +0100

    set up map of cells, since it's required for many future functions

---
+++ class/Cell.lua
@@ -0,0 +1,69 @@
+require 'T-Engine.class'
+
+module("Cell", package.seeall, class.make)
+
+function Cell:init()
+
+    --see what we contain
+    self.actor = nil
+    self.object = {}
+
+    --FOV
+    self.seen = false
+    self.visible = false
+end
+
+--actor/object
+function Cell:getActor()
+    return self.actor
+end
+
+function Cell:setActor(val)
+    self.actor = val
+end
+
+function Cell:getObjects()
+    if #self.object > 0 then return self.object
+    else return false end
+end
+
+function Cell:getNbObjects()
+    if #self.object > 0 then
+        local i = 0
+        --print("[Cell] we have a self.object table")
+        for k,v in pairs(self.object) do
+            i = i + 1
+        end
+        --print("[Cell] Number of objects is", i)
+        return i
+    else 
+        return 0 end
+end
+
+function Cell:getObject(i)
+    return self.object[i]
+end
+
+function Cell:setObject(val, i)
+    if not i then i = 1 end
+    self.object[i] = val
+end
+
+--FOV
+function Cell:isVisible()
+    return self.visible
+end
+
+function Cell:setVisible(val)
+    self.visible = val
+end    
+
+function Cell:isSeen()
+    return self.seen
+end
+
+function Cell:setSeen(val)
+    self.seen = val
+end
+
+return Cell
--- class/Map.lua
+++ class/Map.lua
@@ -1,7 +1,42 @@
 require 'T-Engine.class'
 
+local Cell = require 'class.Cell'
+
 module("Map", package.seeall, class.make)
 
+function Map:init(width, height)
+  print('Map:initialize', width, height)
+    
+    self.cells = {}
+    self.bounds = { X=0, Y=0, Width=width, Height=height }
+
+    -- Initialize the array of cells
+    --Need to start at 1 because that's what Dijkstra wants
+    for x = 1, self.bounds.Width-1 do
+        self.cells[x] = {}
+        for y = 1, self.bounds.Height-1 do
+            self.cells[x][y] = Cell:new()
+        end
+    end
+end
+
+--bounds
+function Map:getBounds()
+--  print('Map:getBounds')
+    return self.bounds
+end
+
+function Map:getWidth()
+--  print('Map:getWidth')
+    return self.bounds.Width
+end
+function Map:getHeight()
+--  print('Map:getHeight')
+    return self.bounds.Height
+end
+
+
+--we're drawing on STI here
 function Map:getCellTerrain(x,y)
     local terrain
 
@@ -12,7 +47,7 @@ function Map:getCellTerrain(x,y)
             local test = data[y][x]
             if test then
                terrain = test.gid
-               print("X "..x..", Y "..y..", terrain is", terrain)
+               --print("X "..x..", Y "..y..", terrain is", terrain)
             end
         end
     end
