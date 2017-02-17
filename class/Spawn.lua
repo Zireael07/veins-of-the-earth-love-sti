fcf76d4b6c29a5c010fa9d9ce14c1991a108c324
Zireael07 <zi@mail.com>
2017-02-17 15:12:59 +0100

    function to spawn objects

--- class/Spawn.lua
+++ class/Spawn.lua
@@ -5,6 +5,7 @@ local TurnManager = require 'class.interface.TurnManager'
 
 local Player = require 'class.Player'
 local NPC = require 'class.NPC'
+local Object = require 'class.Object'
 local Map = require 'class.Map'
 
 module("Spawn", package.seeall, class.make)
@@ -61,6 +62,35 @@ function Spawn:createActor(x,y,id)
 
     --return actor
     return TurnManager:addEntity(actor)
+end
+
+function Spawn:createItem(x,y, id)
+    if not x or not y then print("No location parameters") return end
+    if x > Map:getHeight()-1 then print("X out of bounds") end
+    if y > Map:getWidth()-1 then print("Y out of bounds") end
+    if not Map:getCell(x,y) then print("No cell at x,y") end
+
+    local object
+    if id and object_types[id] then
+      local t = object_types[id]
+      --print("[Spawn] Creating an object from data", id)
+      t = Entity:newEntity(t, "object")
+      --resolve individual stuff
+      if t.setup ~= nil then
+          print("[Spawn] Setup individual item stuff")
+          t:setup(t)       
+      end
+      object = Object.new(t)
+    else
+      print("[Spawn] Id not given, not doing anything") return
+    end
+
+    if object then
+      print_to_log("[Spawn] Created item", object.name, " at ",x,y)
+      object:place(x,y)
+    end
+
+    return object
 end
 
 return Spawn
