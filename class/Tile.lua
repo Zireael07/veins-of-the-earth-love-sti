d31c354639a535b66d469d867a5397df6e8c3155
Zireael07 <zi@mail.com>
2017-02-20 11:07:32 +0100

    add kenney UI elements

--- class/Tile.lua
+++ class/Tile.lua
@@ -18,6 +18,31 @@ function Tile:loadTiles()
 
     --ui
     stone_bg = lg.newImage("gfx/stone_background.png"),
+
+    --more UI
+    ui_left_arrow = lg.newImage("gfx/kenney_ui/arrowBeige_left.png"),
+    ui_right_arrow = lg.newImage("gfx/kenney_ui/arrowBeige_right.png"),
+    ui_platinum_coin = lg.newImage("gfx/kenney_ui/iconCircle_grey.png"),
+    ui_gold_coin = lg.newImage("gfx/kenney_ui/iconCircle_beige.png"),
+    ui_silver_coin = lg.newImage("gfx/kenney_ui/iconCircle_blue.png"),
+    ui_bronze_coin = lg.newImage("gfx/kenney_ui/iconCircle_brown.png"),
+
+    --inventory
+    ammo_inv = lg.newImage("gfx/inventory/ammo_inv.png"),
+    amulet_inv = lg.newImage("gfx/inventory/amulet_inv.png"),
+    armor_inv = lg.newImage("gfx/inventory/armor_inv.png"),
+    arms_inv = lg.newImage("gfx/inventory/arms_inv.png"),
+    belt_inv = lg.newImage("gfx/inventory/belt_inv.png"),
+    boots_inv = lg.newImage("gfx/inventory/boots_inv.png"),
+    cloak_inv = lg.newImage("gfx/inventory/cloak_inv.png"),
+    gloves_inv = lg.newImage("gfx/inventory/gloves_inv.png"),
+    head_inv = lg.newImage("gfx/inventory/head_inv.png"),
+    light_inv = lg.newImage("gfx/inventory/light_inv.png"),
+    mainhand_inv = lg.newImage("gfx/inventory/mainhand_inv.png"),
+    offhand_inv = lg.newImage("gfx/inventory/offhand_inv.png"),
+    ring_inv = lg.newImage("gfx/inventory/ring_inv.png"),
+    shoulder_inv = lg.newImage("gfx/inventory/shoulder_inv.png"),
+    tool_inv = lg.newImage("gfx/inventory/tool_inv.png"),
 }
 
     return loaded_tiles
