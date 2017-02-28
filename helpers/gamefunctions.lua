5bfa90f88baac5231b949bc24d297037dcf02d83
Zireael07 <zi@mail.com>
2017-02-28 14:21:27 +0100

    functions to batch log prints

--- helpers/gamefunctions.lua
+++ helpers/gamefunctions.lua
@@ -36,6 +36,46 @@ function print_to_log(...)
     love.filesystem.append("log.txt", data)
 end
 
+--batch log calls
+function print_later_to_log_call(...)
+    local data
+    --grab all the things
+    for i = 1, select("#", ...) do 
+        if data ~= nil then
+            data = data..tostring(select(i, ...)).." "
+        else
+            data = tostring(select(i, ...)).." "
+        end
+    end
+
+    return data
+end
+
+function print_later_to_log(table, ...)
+    if not table then return end
+    data = print_later_to_log_call(...)
+    table[#table+1] = data
+end
+
+
+function batch_tables_together(dst, src)
+    for i, str in ipairs(src) do
+        dst[#dst+1] = str
+    end
+
+    return dst
+end
+
+function batch_print_to_log(table)
+    local time = os.date("%Y-%m-%d %H:%M:%S", os.time())
+
+    for i, str in ipairs(table) do
+        local data = str
+        data = "[BATCH] ["..time .."] "..data .. "\n"
+        love.filesystem.append("log.txt", data)
+    end
+end
+
 function rng_table(t)
     return t[love.math.random(1,#t)]
 end
